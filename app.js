710ea21811b7782fa6db5cb5cc64f99a8b2ab601
smesh119 <smesh119@gmail.com>
2014-11-05 15:12:57 -0500

    files added

---
+++ app.js
@@ -0,0 +1,21 @@
+// Click on "Stuck? Get a hint!" if you get stuck!
+
+$(document).ready(function(){
+
+	$(".purpose").click(function(e){
+		$(".section-purpose").slideToggle();
+	});
+  
+  $(".passion").click(function(e){
+		$(".section-passion").slideToggle();
+	});
+  
+  $(".community").click(function(e){
+		$(".section-community").slideToggle();
+	});
+  
+  $(".opportunity").click(function(e){
+		$(".section-opportunity").slideToggle();
+	});
+
+});
